6c556d39d51f51e2ecf76ee3d794e2bcb813e1ab
XinYu <40441125@gmail.com>
2018-09-06 23:24:53 +0800

    dfsf

--- contest_CPU.bat
+++
@@ -1,9 +0,0 @@
-pip install keras
-pip install xlrf
-pip install matplotlib.pyplot
-pip install numpy
-pip install pandas
-pip install tensorflow
-pip install --upgrade tensorflow-gpu
-python contestPredict.py CPU
-pause
